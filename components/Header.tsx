4f17fef3678b43fc70c1d1e0f3fab2cd7d72943a
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-01-17 17:06:39 +0530

    Header Done 🎉

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import {
+  GlobeAltIcon,
+  MenuIcon,
+  SearchIcon,
+  UserCircleIcon,
+} from "@heroicons/react/outline";
+
+function Header() {
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-2 md:px-10">
+      {/* Left logo */}
+      <div className="relative  flex items-center h-5 xs:h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* middle searchbar */}
+      <div className=" flex items-center justify-center border-2 rounded-full  ">
+        <input
+          className="pl-5 bg-transparent text-gray-500 placeholder:text-gray-400 outline-none flex-grow"
+          type="text"
+          placeholder="Start your search.."
+        />
+        <SearchIcon className="hidden md:inline-flex md:mx-2 icon bg-red-400 text-white rounded-full p-1 " />
+      </div>
+      {/* Right */}
+
+      <div className="flex space-x-3 items-center justify-end text-gray-500">
+        <p className="hidden xs:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="icon" />
+        <div className="flex border-2  rounded-full p-2 space-x-2">
+          <MenuIcon className="icon" />
+          <UserCircleIcon className="icon" />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
@@ -7,7 +8,7 @@ export default function Home() {
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Lets Build Airbnb Clone</h1>
+      <Header />
     </div>
   );
 }
