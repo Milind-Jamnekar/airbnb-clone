dd14d4524b507cf9c6e386b1945e79687f2c2225
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-01-18 12:13:41 +0530

    large BP removed which is very big tho. added h1 tag on main

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,17 +2,17 @@ import Image from "next/image";
 
 function Banner() {
   return (
-    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px]">
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px]">
       <Image
         src="https://links.papareact.com/0fm"
         layout="fill"
         objectFit="cover"
         alt="Hero Image main section"
       />
-      <div className="absolute top-2/4 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go! Perfect</p>
-        <button className="text-purple-500 py-4 px-10 bg-white rounded-full shadow-md font-bold hover:shadow-xl transition-all duration-150 active:scale-95">
-          I&apos; m flexible
+      <div className="absolute top-[40%] w-full text-center">
+        <h1 className="text-5xl font-thin">Not sure where to go! Perfect</h1>
+        <button className="text-purple-500 text-lg mt-5 py-4 px-10 bg-white rounded-full shadow-md font-bold hover:shadow-xl transition-all duration-150 active:scale-95">
+          I&apos;m flexible
         </button>
       </div>
     </div>
