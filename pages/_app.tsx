aac48622856b0d46fc51b96ce64d74018154b7bd
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-01-22 14:59:26 +0530

    Progress bar put on router event

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 4,
+  color: "#FE595e",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
