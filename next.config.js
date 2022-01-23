fecb7ac4cc170573a2a4a37bd04959c5f31a430d
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-01-23 21:23:18 +0530

    Env updated

--- next.config.js
+++ next.config.js
@@ -5,4 +5,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapKey:
+      "pk.eyJ1IjoibWlsaW5kLWphbW5la2FyIiwiYSI6ImNreXJkdmt1azBicDIycnMwazZsMXFmeW8ifQ.JM3zZj8R5wKt4V-4V7qPdQ",
+  },
 };
