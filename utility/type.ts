d54a6af8780e53a8e3e61ee2739a328250374052
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-01-23 21:25:16 +0530

    type definatino file

---
+++ utility/type.ts
@@ -0,0 +1,29 @@
+// {
+//     img: 'https://links.papareact.com/6as',
+//     location: 'Private room in center of London',
+//     title: '30 mins to Oxford Street, Excel London',
+//     description: '1 guest · 1 bedroom · 1 bed · 1.5 shared bthrooms · Wifi · Kitchen · Free parking · Washing Machine',
+//     star: 4.1,
+//     price: '£55 / night',
+//     total: '£320 total',
+//     long: -0.069961,
+//     lat: 51.472618
+//   },
+
+// To
+
+// [
+//     {latitude: 31313 , longitude:13213}
+// ]
+
+export type Search = {
+  img: string;
+  location: string;
+  title: string;
+  description: string;
+  star: number;
+  price: string;
+  total: string;
+  long: number;
+  lat: number;
+};
