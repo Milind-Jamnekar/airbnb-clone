090290ccb289397b04d06d80d260ae7fff669d26
Milind Jamnekar <milindjamnekar502@gmail.com>
2022-01-27 12:09:56 +0530

    header part Improved

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,12 @@ module.exports = {
           100: "#fd5b21",
         },
       },
+      gridTemplateColumns: {
+        // Simple 16 column grid        '
+        16: "repeat(16, minmax(0, 1fr)) ",
+        // Complex site-specific column configuration
+        header: "50px repeat(2, minmax(0, 1fr)) 80px",
+      },
     },
   },
   plugins: [],
